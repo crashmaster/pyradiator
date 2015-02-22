91a1c95398ead49cf7177943b41f07621d17bfd3
Johnny Crash <cannon.imus@gmail.com>
2015-02-22 22:00:54 +0100

    Extracted command line parameter handling.

---
+++ pyradiator/command_line_args.py
@@ -0,0 +1,136 @@
+import argparse
+import collections
+import sys
+
+import pygame
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+COLORS = collections.OrderedDict([
+    ("white", WHITE),
+    ("black", BLACK),
+])
+
+
+class StoreSize(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, const=None, **kwargs):
+        if const is None:
+            raise ValueError("const for '{}' not set".format(option_strings))
+        super(StoreSize, self).__init__(
+            option_strings, dest, nargs, const, **kwargs
+        )
+
+    def __call__(self, parser, namespace, value, option_string):
+        if value > self.const:
+            raise ValueError("'{}' too big, max: {}".
+                             format(option_string, self.const))
+        setattr(namespace, self.dest, value)
+
+
+class StoreFont(argparse.Action):
+    def __call__(self, parser, namespace, value, option_string):
+        self.print_system_font_list_on_request(value)
+        setattr(namespace, self.dest, value)
+
+    @staticmethod
+    def print_system_font_list_on_request(value):
+        if value == "list":
+            system_fonts = ", ".join(sorted(pygame.font.get_fonts()))
+            print("Available system fonts: {}".format(system_fonts))
+            sys.exit(0)
+
+
+class StoreColor(argparse.Action):
+    def __call__(self, parser, namespace, value, option_string):
+        setattr(namespace, self.dest, COLORS[value])
+
+
+def parse_arguments(display_info):
+    parser = argparse.ArgumentParser(
+        description="THE Radiator.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--fullscreen",
+        action="store_true",
+        default=False,
+        help="Display radiator in fullscreen mode."
+    )
+    parser.add_argument(
+        "--window-width",
+        type=int,
+        default=int(display_info.current_w),
+        action=StoreSize,
+        const=display_info.current_w,
+        help="Width in pixels of the radiator."
+    )
+    parser.add_argument(
+        "--window-height",
+        type=int,
+        action=StoreSize,
+        const=display_info.current_h,
+        default=int(display_info.current_h),
+        help="Height in pixels of the radiator."
+    )
+    parser.add_argument(
+        "--margin-size",
+        type=int,
+        action=StoreSize,
+        const=50,
+        default=5,
+        help="Margin size in pixels of the surfaces."
+    )
+    parser.add_argument(
+        "--main-surface-color",
+        action=StoreColor,
+        default=BLACK,
+        choices=["white", "black"],
+        help="Color of the main surfaces."
+    )
+    parser.add_argument(
+        "--sub-surface-color",
+        action=StoreColor,
+        default=BLACK,
+        choices=["white", "black"],
+        help="Color of the sub-surfaces."
+    )
+    parser.add_argument(
+        "--font",
+        type=str,
+        action=StoreFont,
+        default=pygame.font.get_default_font(),
+        help="Font of the output-text. List system fonts with 'list' value."
+    )
+    parser.add_argument(
+        "--font-size",
+        type=int,
+        default=20,
+        help="Font-size of the output-text."
+    )
+    parser.add_argument(
+        "--font-bold",
+        action="store_true",
+        default=False,
+        help="Font of the output text is bold."
+    )
+    parser.add_argument(
+        "--font-italic",
+        action="store_true",
+        default=False,
+        help="Font of the output text is italic."
+    )
+    parser.add_argument(
+        "--font-fg-color",
+        action=StoreColor,
+        default=WHITE,
+        choices=["white", "black"],
+        help="Foreground color of the fonts."
+    )
+    parser.add_argument(
+        "--font-bg-color",
+        action=StoreColor,
+        default=BLACK,
+        choices=["white", "black"],
+        help="Background color of the fonts."
+    )
+    return parser.parse_args()
--- pyradiator/radiator.py
+++ pyradiator/radiator.py
@@ -1,10 +1,10 @@
-import argparse
-import collections
 import datetime
 import logging
 import sys
 
 import pygame
+
+import command_line_args
 
 IS_HELP_MODE = any(x in sys.argv for x in ["-h", "--help", "list"])
 LOG_LEVEL = logging.ERROR if IS_HELP_MODE else logging.DEBUG
@@ -12,13 +12,6 @@ FORMAT = "%(asctime)-15s %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=LOG_LEVEL)
 
 LOGGER = logging.getLogger("watrad")
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-COLORS = collections.OrderedDict([
-    ("white", WHITE),
-    ("black", BLACK),
-])
 
 
 def initialize_pygame_modules():
@@ -32,129 +25,6 @@ def get_display_info():
     display_info = pygame.display.Info()
     LOGGER.debug("Display info: \n{}".format(display_info).rstrip())
     return display_info
-
-
-class StoreSize(argparse.Action):
-    def __init__(self, option_strings, dest, nargs=None, const=None, **kwargs):
-        if const is None:
-            raise ValueError("const for '{}' not set".format(option_strings))
-        super(StoreSize, self).__init__(
-            option_strings, dest, nargs, const, **kwargs
-        )
-
-    def __call__(self, parser, namespace, value, option_string):
-        if value > self.const:
-            raise ValueError("'{}' too big, max: {}".
-                             format(option_string, self.const))
-        setattr(namespace, self.dest, value)
-
-
-class StoreFont(argparse.Action):
-    def __call__(self, parser, namespace, value, option_string):
-        self.print_system_font_list_on_request(value)
-        setattr(namespace, self.dest, value)
-
-    @staticmethod
-    def print_system_font_list_on_request(value):
-        if value == "list":
-            system_fonts = ", ".join(sorted(pygame.font.get_fonts()))
-            print("Available system fonts: {}".format(system_fonts))
-            sys.exit(0)
-
-
-class StoreColor(argparse.Action):
-    def __call__(self, parser, namespace, value, option_string):
-        setattr(namespace, self.dest, COLORS[value])
-
-
-def parse_arguments(display_info):
-    parser = argparse.ArgumentParser(
-        description="THE Radiator.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        "--fullscreen",
-        action="store_true",
-        default=False,
-        help="Display radiator in fullscreen mode."
-    )
-    parser.add_argument(
-        "--window-width",
-        type=int,
-        default=int(display_info.current_w),
-        action=StoreSize,
-        const=display_info.current_w,
-        help="Width in pixels of the radiator."
-    )
-    parser.add_argument(
-        "--window-height",
-        type=int,
-        action=StoreSize,
-        const=display_info.current_h,
-        default=int(display_info.current_h),
-        help="Height in pixels of the radiator."
-    )
-    parser.add_argument(
-        "--margin-size",
-        type=int,
-        action=StoreSize,
-        const=50,
-        default=5,
-        help="Margin size in pixels of the surfaces."
-    )
-    parser.add_argument(
-        "--main-surface-color",
-        action=StoreColor,
-        default=BLACK,
-        choices=["white", "black"],
-        help="Color of the main surfaces."
-    )
-    parser.add_argument(
-        "--sub-surface-color",
-        action=StoreColor,
-        default=BLACK,
-        choices=["white", "black"],
-        help="Color of the sub-surfaces."
-    )
-    parser.add_argument(
-        "--font",
-        type=str,
-        action=StoreFont,
-        default=pygame.font.get_default_font(),
-        help="Font of the output-text. List system fonts with 'list' value."
-    )
-    parser.add_argument(
-        "--font-size",
-        type=int,
-        default=20,
-        help="Font-size of the output-text."
-    )
-    parser.add_argument(
-        "--font-bold",
-        action="store_true",
-        default=False,
-        help="Font of the output text is bold."
-    )
-    parser.add_argument(
-        "--font-italic",
-        action="store_true",
-        default=False,
-        help="Font of the output text is italic."
-    )
-    parser.add_argument(
-        "--font-fg-color",
-        action=StoreColor,
-        default=WHITE,
-        choices=["white", "black"],
-        help="Foreground color of the fonts."
-    )
-    parser.add_argument(
-        "--font-bg-color",
-        action=StoreColor,
-        default=BLACK,
-        choices=["white", "black"],
-        help="Background color of the fonts."
-    )
-    return parser.parse_args()
 
 
 def create_main_surface(args):
@@ -215,13 +85,13 @@ def loop(args, subsurfaces, font):
             elif event.type == display_refresh:
                 pygame.display.flip()
 
-        for i in subsurfaces:
-            i.fill(args.sub_surface_color)
+        for subsurface in subsurfaces:
+            subsurface.fill(args.sub_surface_color)
             time = font.render(str(datetime.datetime.now()),
                                True,
                                args.font_fg_color,
                                args.font_bg_color)
-            i.blit(time, (0, 0))
+            subsurface.blit(time, (0, 0))
 
         pygame.time.wait(0)
 
@@ -229,7 +99,7 @@ def loop(args, subsurfaces, font):
 def main():
     initialize_pygame_modules()
     display_info = get_display_info()
-    args = parse_arguments(display_info)
+    args = command_line_args.parse_arguments(display_info)
     main_surface = create_main_surface(args)
     subsurfaces = create_sub_surfaces(args, main_surface)
     font = create_font(args)
