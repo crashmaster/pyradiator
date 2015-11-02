a9297455de2bbade623ab42ec14c7fcaf6110af2
Johnny Crash <cannon.imus@gmail.com>
2015-11-02 08:16:22 +0100

    Refactor

--- pyradiator/common.py
+++ pyradiator/common.py
@@ -49,7 +49,7 @@ class PrintText(object):
             for line_part in line:
                 rendered_text = self.font.render(
                     line_part.text,
-                    1 if self.config.font_antialias else 0,
+                    int(self.config.font_antialias),
                     line_part.color
                 )
                 text_surface.blit(rendered_text, (position_x, position_y))
